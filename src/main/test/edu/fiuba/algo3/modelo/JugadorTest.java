150d77c2cb8a423ff5f6695ac3f480e8b70c84a8
Nicolas Zulaica <nzulaica@fi.uba.ar>
2021-07-04 16:02:33 -0300

    Jugador

--- src/main/test/edu/fiuba/algo3/modelo/JugadorTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/JugadorTest.java
@@ -1,17 +1,69 @@
 package edu.fiuba.algo3.modelo;
 
-import edu.fiuba.algo3.Jugador;
+import edu.fiuba.algo3.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 public class JugadorTest {
 
-    Jugador jugador = new Jugador();
+    Jugador jugador;
+    Pais atacante;
+    Pais defensor;
+
+    @BeforeEach
+    public void setUp(){
+        jugador = new Jugador();
+        atacante = mock(Pais.class);
+        defensor = mock(Pais.class);
+    }
 
     @Test
-    public void testDePrueba(){
+    public void JugadorNoPuedeAtacarConUnPaisQueNoTiene(){
 
-        assertEquals(jugador.invadir(null,null), 0);
+        assertThrows(JugadorNoTienePais.class, () -> jugador.atacar(atacante,defensor,1));
+    }
+
+    @Test
+    public void JugadorNoPuedeAtacarAUnPaisQueYaTiene(){
+
+        jugador.ocupar(defensor);
+        jugador.ocupar(atacante);
+
+        assertThrows(PaisDelMismoPropietarioNoPuedeSerAtacado.class, () -> jugador.atacar(atacante,defensor,1));
+    }
+
+    @Test
+    public void JugadorPuedeAtacarUnPais(){
+
+        jugador.ocupar(atacante);
+
+        for( int i=0; i<=4; i++ ){
+            jugador.atacar(atacante,defensor,i);
+            verify(atacante,times(1)).atacar(defensor,i);
+        }
+    }
+
+    @Test
+    public void JugadorPuedeAtacarAUnPaisQueDesocupa(){
+
+        jugador.ocupar(defensor);
+        jugador.ocupar(atacante);
+        jugador.desocupar(defensor);
+        jugador.atacar(atacante,defensor,2);
+        verify(atacante,times(1)).atacar(defensor,2);
+    }
+
+    @Test
+    public void JugadorNoPuedeAtacarConUnPaisQueDesocupa(){
+
+        jugador.ocupar(atacante);
+        jugador.desocupar(atacante);
+
+        assertThrows(JugadorNoTienePais.class, () -> jugador.atacar(atacante,defensor,1));
     }
 }
