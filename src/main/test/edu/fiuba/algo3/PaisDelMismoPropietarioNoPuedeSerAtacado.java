775b9e738374acac5220b8a94d716e042ce37eb5
Nicolas Zulaica <66538092+NicolasEzequielZulaicaRivera@users.noreply.github.com>
2021-07-08 18:40:09 -0300

    Delete PaisDelMismoPropietarioNoPuedeSerAtacado.java

--- src/main/test/edu/fiuba/algo3/PaisDelMismoPropietarioNoPuedeSerAtacado.java
+++
@@ -1,7 +0,0 @@
-package edu.fiuba.algo3;
-
-public class PaisDelMismoPropietarioNoPuedeSerAtacado extends RuntimeException {
-    public PaisDelMismoPropietarioNoPuedeSerAtacado(String mensaje) {
-        super(mensaje);
-    }
-}
