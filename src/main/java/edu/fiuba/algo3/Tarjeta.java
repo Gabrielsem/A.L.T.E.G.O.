3c26f0b0c2d8c29b7a3ce5eb42a8ebb7fd8efb3b
Nicolas Zulaica <nzulaica@fi.uba.ar>
2021-07-12 18:39:28 -0300

    inicio Tarjeta

---
+++ src/main/java/edu/fiuba/algo3/Tarjeta.java
@@ -0,0 +1,25 @@
+package edu.fiuba.algo3;
+
+import java.util.Collection;
+
+public class Tarjeta {
+
+
+    // TODO - Ari
+    public Tarjeta(){
+    }
+
+    // Cuando una tarjeta se activa se suman 2 ejercitos al pais
+    // Solo se puede activar una vez ( cada vez que es repartida )
+    public void activar(){
+        //TODO
+    }
+
+    // Recibe una colleccion de tarjetas y devuelve un grupo de 3 tarjetas canjeable si hay, null si no hay
+    // Un grupo canjeable son 3 iguales o 3 diferentes
+    static public Collection<Tarjeta> grupoCanjeable( Collection<Tarjeta> tarjetas ){
+        //TODO
+        return null;
+    }
+
+}
