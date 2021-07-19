5987be28ac9bb61be1a36d00de70ecef0275eceb
Nicolas Zulaica <nzulaica@fi.uba.ar>
2021-07-18 23:36:12 -0300

    probando JavaFX

--- src/main/java/edu/fiuba/algo3/vista/App.java
+++ src/main/java/edu/fiuba/algo3/vista/App.java
@@ -1,10 +1,14 @@
 package edu.fiuba.algo3.vista;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -32,10 +36,58 @@ public class App extends Application {
 
         StackPane layout = new StackPane();
 
-        Button botonJugar = new Button("Jugar");
-        Button botonReglas = new Button("Reglas");
+        VBox contenedor = new VBox();
+        contenedor.setSpacing(10);
+        contenedor.setAlignment(Pos.TOP_CENTER);
 
-        layout.getChildren().add(botonJugar);
+        Label titulo = new Label("Menu Principal");
+        contenedor.getChildren().add(titulo);
+
+        Button botonJugar = new Button("Jugar");
+        botonJugar.setOnAction( e->{
+            System.out.println("Jugar");
+            seleccionarJugadores(stage);
+        } );
+        contenedor.getChildren().add(botonJugar);
+
+        Button botonReglas = new Button("Reglas");
+        botonReglas.setOnAction( e->{
+            System.out.println("Reglas");
+        } );
+        contenedor.getChildren().add(botonReglas);
+
+        layout.getChildren().add(contenedor);
+
+        var scene = new Scene(layout, 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void seleccionarJugadores( Stage stage ) {
+
+        StackPane layout = new StackPane();
+
+        VBox contenedor = new VBox();
+        contenedor.setSpacing(10);
+        contenedor.setAlignment(Pos.TOP_CENTER);
+
+        Label titulo = new Label("Seleccionar cantidad de Jugadores");
+        contenedor.getChildren().add(titulo);
+
+        HBox opciones = new HBox();
+        opciones.setSpacing(10);
+        opciones.setAlignment(Pos.TOP_CENTER);
+
+        for( int i=2; i<=6; i++ ) {
+            Button boton = new Button(""+i);
+            BotonIniciarJuego handler = new BotonIniciarJuego(i);
+            boton.setOnAction(handler);
+            opciones.getChildren().add(boton);
+        }
+
+        contenedor.getChildren().add(opciones);
+
+        layout.getChildren().add(contenedor);
 
         var scene = new Scene(layout, 640, 480);
         stage.setScene(scene);
---
+++ src/main/java/edu/fiuba/algo3/vista/BotonIniciarJuego.java
@@ -0,0 +1,18 @@
+package edu.fiuba.algo3.vista;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class BotonIniciarJuego implements EventHandler<ActionEvent> {
+
+    int cantidadDeJugadores;
+
+    public BotonIniciarJuego(int cantJugadores ) {
+        cantidadDeJugadores = cantJugadores;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        System.out.println("Comenzar Juego con "+cantidadDeJugadores+" jugadores");
+    }
+}
