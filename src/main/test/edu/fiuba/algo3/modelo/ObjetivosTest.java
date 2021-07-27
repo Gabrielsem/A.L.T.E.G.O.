639537523716e9f36918100b1e48a1d4a46adaa5
Marcelo <marcearielrondan@gmail.com>
2021-07-27 17:51:23 -0300

    Test objetivos ocupacion y destruccion

--- src/main/test/edu/fiuba/algo3/modelo/ObjetivosTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/ObjetivosTest.java
@@ -5,11 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.mock;
 
 public class ObjetivosTest {
@@ -69,12 +66,16 @@ public class ObjetivosTest {
         assert(!objetivo.gano(jug1));
 
     }
-    /*
+
     @Test
     public void jugadorCumpleObjetivoOcupacion() {
         HashMap <String, Integer> cantidades = new HashMap<>();
-        cantidades.put("Oceania", 2);
-        cantidades.put("Asia", 3);
+        int contOceania = 2;
+        int contAsia = 3;
+        cantidades.put("Oceania", contOceania);
+        cantidades.put("Asia", contAsia);
+
+        //Creamos el objetivo con ciertas cantidades predeterminadas
         ObjetivoOcupacion objetivo = new ObjetivoOcupacion(cantidades, mapa);
 
         Juego juego = mock(Juego.class);
@@ -83,15 +84,141 @@ public class ObjetivosTest {
         Jugador jug2 = new Jugador(2, juego);
         ArrayList<Pais> paisesDelMapa = mapa.obtenerPaises();
 
+        //Ocupamos todos los paises por el jugador 2 para que no estén sin propietario
         for(Pais pais: paisesDelMapa) {
             jug2.ocupar(pais);
             pais.ocupadoPor(jug2, 1);
         }
 
+        //Ocupamos la cantidad de paises indicada para ganar, con el jugador 1
         for (Pais pais: paisesDelMapa) {
-
+            if (pais.continente().equals("Oceania") && contOceania > 0) {
+                jug1.ocupar(pais);
+                pais.ocupadoPor(jug1, 1);
+                contOceania--;
+            }
+            if (pais.continente().equals("Asia") && contAsia > 0) {
+                jug1.ocupar(pais);
+                pais.ocupadoPor(jug1, 1);
+                contAsia--;
+            }
         }
+
+        assert(objetivo.gano(jug1));
     }
 
-     */
+    @Test
+    public void jugadorCumpleObjetivoOcupacionConPaisesDeMas() {
+        HashMap <String, Integer> cantidades = new HashMap<>();
+        int contOceania = 2;
+        int contAsia = 3;
+        cantidades.put("Oceania", contOceania);
+        cantidades.put("Asia", contAsia);
+
+        //Creamos el objetivo con ciertas cantidades predeterminadas
+        ObjetivoOcupacion objetivo = new ObjetivoOcupacion(cantidades, mapa);
+
+        Juego juego = mock(Juego.class);
+
+        Jugador jug1 = new Jugador(1, juego);
+        Jugador jug2 = new Jugador(2, juego);
+        ArrayList<Pais> paisesDelMapa = mapa.obtenerPaises();
+
+        //Ocupamos todos los paises por el jugador 2 para que no estén sin propietario
+        for(Pais pais: paisesDelMapa) {
+            jug2.ocupar(pais);
+            pais.ocupadoPor(jug2, 1);
+        }
+
+        //Ocupamos una cantidad de paises mayor a la necesaria para ganar, con el jugador 1
+        for (Pais pais: paisesDelMapa) {
+            if (pais.continente().equals("Oceania") || pais.continente().equals("Asia") || pais.continente().equals("Africa")) {
+                jug1.ocupar(pais);
+                pais.ocupadoPor(jug1, 1);
+            }
+        }
+
+        assert(objetivo.gano(jug1));
+    }
+
+    @Test
+    public void jugadorNoCumpleObjetivoOcupacion() {
+        HashMap <String, Integer> cantidades = new HashMap<>();
+        int contOceania = 2;
+        int contAsia = 3;
+        cantidades.put("Oceania", contOceania);
+        cantidades.put("Asia", contAsia);
+
+        //Creamos el objetivo con ciertas cantidades predeterminadas
+        ObjetivoOcupacion objetivo = new ObjetivoOcupacion(cantidades, mapa);
+
+        Juego juego = mock(Juego.class);
+
+        Jugador jug1 = new Jugador(1, juego);
+        Jugador jug2 = new Jugador(2, juego);
+        ArrayList<Pais> paisesDelMapa = mapa.obtenerPaises();
+
+        //Ocupamos todos los paises por el jugador 2 para que no estén sin propietario
+        for(Pais pais: paisesDelMapa) {
+            jug2.ocupar(pais);
+            pais.ocupadoPor(jug2, 1);
+        }
+
+        //Ocupamos la cantidad de paises menor a la necesaria para ganar, con el jugador 1
+        for (Pais pais: paisesDelMapa) {
+            if (pais.continente().equals("Oceania") && contOceania > 0) {
+                jug1.ocupar(pais);
+                pais.ocupadoPor(jug1, 1);
+                contOceania--;
+            }
+            if (pais.continente().equals("Asia") && contAsia > 1) {
+                jug1.ocupar(pais);
+                pais.ocupadoPor(jug1, 1);
+                contAsia--;
+            }
+        }
+
+        assert(!objetivo.gano(jug1));
+    }
+
+    @Test
+    public void jugadorGanaAlDestruirAJugadorObjetivo(){
+
+        Juego juego = mock(Juego.class);
+
+        Jugador jug1 = new Jugador(1, juego);
+        Jugador jug2 = new Jugador(2, juego);
+
+        ObjetivoDestruccion objetivo = new ObjetivoDestruccion(jug2, mapa);
+        ArrayList<Pais> paisesDelMapa = mapa.obtenerPaises();
+
+        for(Pais pais: paisesDelMapa) {
+            jug1.ocupar(pais);
+            pais.ocupadoPor(jug1, 1);
+        }
+
+        assert(objetivo.gano(jug1));
+    }
+
+    @Test
+    public void jugadorNoGanaPorNoDestruirAJugadorObjetivo(){
+
+        Juego juego = mock(Juego.class);
+
+        Jugador jug1 = new Jugador(1, juego);
+        Jugador jug2 = new Jugador(2, juego);
+
+        ObjetivoDestruccion objetivo = new ObjetivoDestruccion(jug2, mapa);
+        ArrayList<Pais> paisesDelMapa = mapa.obtenerPaises();
+
+        for(Pais pais: paisesDelMapa) {
+            jug1.ocupar(pais);
+            pais.ocupadoPor(jug1, 1);
+        }
+
+        jug2.ocupar(paisesDelMapa.get(0));
+        paisesDelMapa.get(0).ocupadoPor(jug2, 1);
+
+        assert(!objetivo.gano(jug1));
+    }
 }
