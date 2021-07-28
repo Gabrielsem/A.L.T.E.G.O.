daa14bd4ee6ca52d4209dd7f1db7399d1792847a
ariana-salese <asalese@fi.uba.ar>
2021-07-28 16:36:46 -0300

    actualizacion vistaPedirJugadores

--- src/main/java/edu/fiuba/algo3/interfaz/ControladorPantallaPedirJugadores.java
+++ src/main/java/edu/fiuba/algo3/interfaz/ControladorPantallaPedirJugadores.java
@@ -8,6 +8,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Observer;
@@ -15,21 +17,29 @@ import java.util.Observer;
 public class ControladorPantallaPedirJugadores {
 
     Scene scene;
+    HashMap<Integer, String> coloresJugadores;
 
     public ControladorPantallaPedirJugadores(Scene scene) throws IOException {
         this.scene = scene;
+        this.coloresJugadores = new HashMap<>();
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("VistaPantallaPedirJugadores.fxml"));
         fxmlLoader.setController(this);
         scene.setRoot(fxmlLoader.load());
     }
 
     @FXML
+    public void agregarJugador(ActionEvent actionEvent) {
+        Button botonColor = (Button) actionEvent.getSource();
+        System.out.println(((Button) actionEvent.getSource()).getAccessibleText());
+        coloresJugadores.put(this.coloresJugadores.size() + 1, botonColor.getAccessibleText());
+        botonColor.setOnAction(null);
+        botonColor.setOpacity(0.5);
+    }
+
+    @FXML
     public void iniciarJuego(ActionEvent actionEvent) throws IOException {
         System.out.println("Comenzar Juego");
-
-        int cantJ = 3; // TODO -
-        Juego juego = new Juego(cantJ, "archivos/paises.json", "objetivos.json");
-
+        Juego juego = new Juego(this.coloresJugadores.size(), "archivos/paises.json", "objetivos.json");
         new ControladorPantallaJuego(scene, juego);
         addPaisObservers(juego);
     }
@@ -41,7 +51,7 @@ public class ControladorPantallaPedirJugadores {
             if (nodo.getId().equals("fondo") || nodo.getId().startsWith("puente")) continue;
             observers.put(nodo.getId(), new VistaPais(nodo));*/
             if (nodo.getStyleClass().contains("pais")) {
-                observers.put(nodo.getId(), new VistaPais(nodo));
+                observers.put(nodo.getId(), new VistaPais(nodo, this.coloresJugadores.get(juego.obtenerPais(nodo.getId()).getNumeroPropietario())));
             }
         }
 
