a00bd7298c40555883f430c05370b68a5db54eac
ariana-salese <asalese@fi.uba.ar>
2021-07-28 23:04:55 -0300

    actualizacion vistas

--- src/main/java/edu/fiuba/algo3/interfaz/fases/GestorColocacion.java
+++ src/main/java/edu/fiuba/algo3/interfaz/fases/GestorColocacion.java
@@ -8,7 +8,6 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Button;
 
 public class GestorColocacion implements Fase {
     Juego juego;
