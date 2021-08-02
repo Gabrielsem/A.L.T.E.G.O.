99e1cfa6da494ad6d444361cb34f9a27747d2398
Nicolas Zulaica <nzulaica@fi.uba.ar>
2021-08-02 14:22:35 -0300

    eeeee

--- src/main/java/edu/fiuba/algo3/interfaz/VistaJugador.java
+++ src/main/java/edu/fiuba/algo3/interfaz/VistaJugador.java
@@ -7,8 +7,15 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Observable;
@@ -81,12 +88,32 @@ public class VistaJugador implements Observer {
     private void actualizarTarjetas(Jugador jugador) {
         cajaTarjetas.getChildren().clear();
         Collection<Tarjeta> tarjetas = jugador.getTarjetas();
-        String texto = "";
+
+        VBox caja = new VBox();
+        caja.getStyleClass().add("tarjetas");
+
+        cajaTarjetas.getChildren().add(caja);
+
         for (Tarjeta t : tarjetas) {
-            texto = texto.concat(String.format("%s - %s\n", t.obtenerSimbolo().obtenerNombre(), t.pais()));
+
+            StackPane tarjeta = new StackPane();
+            tarjeta.getStyleClass().add("tarjeta");
+            ImageView imagen = new ImageView();
+            HBox pais = new HBox();
+
+            try {
+                imagen = new ImageView(new Image(new FileInputStream("imagenes/globo.png")));
+                //String texto = String.format("%s - %s\n", t.obtenerSimbolo().obtenerNombre(), t.pais());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            pais.getChildren().add( new Label( t.pais() ) );
+
+
+            tarjeta.getChildren().addAll(imagen,pais);
+            caja.getChildren().add(tarjeta);
         }
-        Label objetivo = new Label(texto); //El label no debería llamarse tarjetas?
-        cajaTarjetas.getChildren().add(objetivo);
     }
 
     private void actualizarObjetivos(Jugador jugador) {
