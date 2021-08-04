101faa8c9f29ec084bd9de6e5a839a424d41514b
Nicolas Zulaica <nzulaica@fi.uba.ar>
2021-08-04 17:23:16 -0300

    popup batalla

---
+++ src/main/java/edu/fiuba/algo3/interfaz/vistas/VistaBatalla.java
@@ -0,0 +1,33 @@
+package edu.fiuba.algo3.interfaz.vistas;
+
+import edu.fiuba.algo3.App;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class VistaBatalla {
+    public VistaBatalla(int[] dadosAtacante, int[] dadosDefensor) {
+
+        VBox caja = new VBox();
+        caja.getStyleClass().add("Batalla");
+        HBox cajaTitulo = new HBox( new Label("BATALLA") );
+        HBox cajaAtacante = new HBox( new Label("Atacante: ") );
+        HBox cajaDefensor = new HBox( new Label("Defensor: ") );
+        caja.getChildren().addAll(cajaTitulo,cajaAtacante,cajaDefensor);
+
+        for (int j : dadosAtacante) {
+            cajaAtacante.getChildren().add(new Label(String.valueOf(j)));// TODO - Imagenes
+        }
+
+        for( int j : dadosDefensor ){
+            cajaDefensor.getChildren().add( new Label( String.valueOf( j ) ) );// TODO - Imagenes
+        }
+
+        caja.setPadding( new Insets(10) );
+        cajaAtacante.setPadding( new Insets(10) );
+        cajaDefensor.setPadding( new Insets(10) );
+
+        App.popUpWindow( caja );
+    }
+}
